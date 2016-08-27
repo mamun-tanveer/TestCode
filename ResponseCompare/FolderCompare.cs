fc342802087bbcf1e1c9317e4daefd0e377504e9
mamun-tanveer <mamun.tanveer@outlook.com>
2016-08-27 16:43:46 -0500

    Initial ResponseCompare stuff

---
+++ ResponseCompare/FolderCompare.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ResponseCompare
+{
+    
+    public class FolderCompare
+    {
+        private IEnumerable<string> files1;
+        private IEnumerable<string> files2;
+        private IFileCompare comparer; 
+
+        public FolderCompare(string folder1, string folder2)
+        {
+            files1 = Directory.GetFiles(folder1);
+            files2 = Directory.GetFiles(folder2);   
+        }
+
+        public bool Compare()
+        {
+            bool returnValue = true;
+            var intersect = new HashSet<string>(files1.Intersect(files2));
+
+            returnValue = (intersect.Count == files1.Count());
+
+            return returnValue;
+        }
+    }
+}
---
+++ ResponseCompare/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponseCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool inputValid = false;
+            string folder1 = string.Empty;
+            string folder2 = string.Empty;
+            if (args.Length >= 2)
+            {
+                folder1 = args[0];
+                folder2 = args[1];
+
+                inputValid = (validateFolder(folder1) && validateFolder(folder2));
+            } 
+            
+            if (!inputValid)
+            {
+                getInput(out folder1, out folder2);
+            }
+        }
+
+        static void getInput(out string x, out string y)
+        {
+            Console.WriteLine("Input first folder");
+            x = Console.ReadLine();
+
+            Console.WriteLine("Input second folder");
+            y = Console.ReadLine();
+
+            if (!validateFolder(x) || !validateFolder(y))
+            {
+                getInput(out x, out y);
+            }       
+        }
+
+        static bool validateFolder(string path)
+        {
+
+            bool returnValue = System.IO.Directory.Exists(path);
+            if (returnValue == false)
+            {
+                Console.WriteLine(path + " doesn't exist");
+            }
+            return returnValue;
+        }
+
+        
+                
+    }
+}
