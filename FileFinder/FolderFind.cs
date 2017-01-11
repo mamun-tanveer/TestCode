db3d398452072590401d9a32606a50bb6215a05c
mamun <mamun.tanveer@mcmaster.com>
2017-01-10 22:49:16 -0600

    File Finder

---
+++ FileFinder/FolderFind.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileFinder
+{
+    class FolderFind
+    {
+        public IDictionary<string, List<string>> FileLocations { get; private set; }
+        public int MaxRecursionLevel { get; set; } = 10; 
+
+        public FolderFind(string rootPath, bool includeSubFolders = true)
+        {
+            FileLocations = new Dictionary<string, List<string>>();
+            if(includeSubFolders)
+            {
+                recursiveAdd(rootPath,  0);
+            }
+            else
+            {
+                addFileLocations(rootPath);
+            } 
+        }
+
+        private void recursiveAdd(string rootPath, int recurseLevel)
+        {
+
+            addFileLocations(rootPath);         
+            if(recurseLevel < MaxRecursionLevel)
+            {
+                foreach(string dirPath in Directory.GetDirectories(rootPath))
+                {
+                    recursiveAdd(dirPath, recurseLevel + 1);
+                }
+            }
+        }
+
+        private void addFileLocations(string folderPath)
+        {
+            Console.WriteLine("Processing folder " + folderPath);
+            foreach(string filePath in Directory.GetFiles(folderPath))
+            {
+                string filename = Path.GetFileName(filePath);
+                if (FileLocations.ContainsKey(filename) == false)
+                {
+                    //never encountered before
+                    FileLocations.Add(filename, new List<string>());
+                }
+                FileLocations[filename].Add(folderPath);
+            }
+        }
+    }
+}
---
+++ FileFinder/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileFinder
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string inputFolderPath = string.Empty;
+            string outputFilePath = string.Empty;
+            getInputArgs(args, out inputFolderPath, out outputFilePath);
+
+            var finder = new FolderFind(inputFolderPath);
+            writeOutputFile(outputFilePath, finder.FileLocations);            
+        }
+
+        static void getInputArgs(string[] args, out string inputFolderPath, out string outputFilePath)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Input Folder Path");
+                inputFolderPath = Console.ReadLine();
+                Console.WriteLine("Output File Path");
+                outputFilePath = Console.ReadLine();
+            }
+            else if (args.Length == 1)
+            {
+                inputFolderPath = args[0];
+                Console.WriteLine("Output File Path");
+                outputFilePath = Console.ReadLine();
+            }
+            else
+            {
+                inputFolderPath = args[0];
+                outputFilePath = args[1];
+            }
+
+            if(Directory.Exists(inputFolderPath) == false)
+            {
+                throw new DirectoryNotFoundException(inputFolderPath + "does not exist");
+            }
+        }
+
+        static void writeOutputFile(string outputFilePath, IDictionary<string, List<string>> locationMap)
+        {
+            using (var writer = new StreamWriter(outputFilePath))
+            {
+                foreach(string filename in locationMap.Keys)
+                {
+                    foreach(string location in locationMap[filename])
+                    {
+                        writer.WriteLine(filename + "\t" + location);
+                    }
+                }
+            }
+        }
+
+    }
+}
