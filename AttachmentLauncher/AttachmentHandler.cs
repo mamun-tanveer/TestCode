9798409466a27c554e14e961db2eee15296077dc
mamun-tanveer <mamun.tanveer@outlook.com>
2020-05-13 03:27:42 -0500

    AttachmentLauncher

---
+++ AttachmentLauncher/AttachmentHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttachmentLauncher
+{
+    internal class AttachmentHandler
+    {
+        private string mFilePath;
+        
+        public AttachmentHandler(string attachmentFilePath)
+        {
+            mFilePath = attachmentFilePath;
+        }
+
+        public string GetAttachmentFile()
+        {
+            return parseFile();
+        }
+
+        private string parseFile()
+        {
+            using (var reader = new System.IO.StreamReader(mFilePath))
+            {
+                return reader.ReadLine();
+            }
+        }
+    }
+}
---
+++ AttachmentLauncher/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AttachmentLauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = string.Empty;
+                if (args.Length == 0)
+                {
+                    path = getInput();
+                }
+                else
+                {
+                    path = args[0];
+                }
+
+                string file = string.Empty;
+                if (Path.GetExtension(path) == ".attach")
+                {
+                    var handler = new AttachmentHandler(path);
+                    file = handler.GetAttachmentFile();
+                }
+                else file = path;
+
+                System.Diagnostics.Process.Start(file);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed! Contact Support");
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+
+        static string getInput()
+        {
+            Console.WriteLine("Invalid Path provided. Input Path");
+            string returnPath = Console.ReadLine();
+            if (!File.Exists(returnPath)) returnPath = getInput();
+            return returnPath;            
+        }
+    }
+}
